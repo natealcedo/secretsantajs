1ca02a57603a57f4718061a649faa637022cddaf
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2017-08-19 21:37:18 +0800

    Add better logging

--- src/app/index.js
+++ src/app/index.js
@@ -2,15 +2,19 @@ import TeleBot from "telebot";
 import { TELEBOT_CONFIG } from "config";
 import { logger } from "lib/utils";
 
-const bot = new TeleBot(process.env.TELEGRAM_TOKEN);
+const telegramBot = new TeleBot(process.env.TELEGRAM_TOKEN);
 
-bot.on("start", () => {
+telegramBot.on("start", () => {
   logger.info("Bot now polling for updates...");
 });
 
-bot.on("text", msg => {
-  logger.info(`User sent: ${msg}`);
-  msg.reply.text(msg.text);
+telegramBot.on("text", message => {
+  logger.info(`
+  user: ${message.from.username}
+  date-time: ${message.date}
+  chat-type: ${message.chat.type}
+  `);
+  message.reply.text(message.text);
 });
 
-export default bot;
+export default telegramBot;
