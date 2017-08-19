a3b681cda67e2936fffd7a45b0a230962b6df37f
Liau Jian Jie <hello@jianjie.co>
2017-08-19 20:16:18 +0800

    Add scaffold controller functions

---
+++ src/app/controllers/index.js
@@ -0,0 +1,19 @@
+const createGroup = () => {};
+const addUserToGroup = () => {};
+const removeUserFromGroup = () => {};
+const assignGiftRecipient = () => {};
+const giftRecipientList = () => {};
+const endDate = () => {};
+const setEndDate = () => {};
+const userList = () => {};
+
+export default {
+  createGroup,
+  addUserToGroup,
+  removeUserFromGroup,
+  assignGiftRecipient,
+  giftRecipientList,
+  endDate,
+  setEndDate,
+  userList,
+};
