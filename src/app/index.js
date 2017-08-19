6d32ef96609d8b96b4ad4151e3bd024bca52d5f9
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2017-08-19 21:47:07 +0800

    Edit formatting

--- src/app/index.js
+++ src/app/index.js
@@ -8,11 +8,13 @@ telegramBot.on("start", () => {
   logger.info("Bot now polling for updates...");
 });
 
+// Sample logging. Add what you think should get logged.
 telegramBot.on("text", message => {
   logger.info(`
-  user: ${message.from.username}
-  date-time: ${message.date}
-  chat-type: ${message.chat.type}
+user: ${message.from.username}
+date-time: ${message.date}
+chat-type: ${message.chat.type}
+message-received: ${message.text}
   `);
   message.reply.text(message.text);
 });
