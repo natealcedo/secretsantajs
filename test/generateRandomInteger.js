3cff6f0de276f0c4f8a475eeb20e059f5c411545
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2017-08-20 22:32:13 +0800

    Add a test case to ensure that only integers are being produced by generateRandomInteger function. Also break out SETUP test suite out of integer test suite

--- test/generateRandomInteger.js
+++ test/generateRandomInteger.js
@@ -3,10 +3,22 @@ import { generateRandomInteger } from "lib/utils";
 
 /* eslint-disable no-undef, no-alert, no-console */
 
-describe("Generate Random Number Integer", () => {
-  it("Mocha plus chai should be working as is", () => {
+describe("MOCHA SETUP WITH CHAI", () => {
+  it("Mocha plus chai should not shit the bed", () => {
     const number = 2;
     expect(number).to.equal(2);
+  });
+});
+
+describe("Generate Random Number Integer", () => {
+  it("generateRandomInteger should actually only generate integers", () => {
+    const upper = 100;
+    const lower = 0;
+    for (let i = 0; i <= 1000; i += 1) {
+      const generatedNumber = generateRandomInteger(lower, upper);
+      expect(generatedNumber % 1 === 0);
+      expect(generatedNumber % generatedNumber === 1);
+    }
   });
 
   it("generateRandomNumberInteger should NEVER generate a number outside the limits of upper and lower bounds", () => {
