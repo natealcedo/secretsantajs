589c514710c2c4ca5f6e57b131fb44de3864a291
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2017-08-20 21:10:28 +0800

    Move back controllers out of telegram directory. Reason behind is that controllers should be interface blind. A controller called from a whatsapp interface should be the same as a controller called from a telegram interface. The logic of how to handle a user request should instead lie in the interface.

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -1,7 +1,30 @@
-import telegram from "./telegram";
+import addUserToGroup from "./addUserToGroup";
+import assignGiftRecipient from "./assignGiftRecepient";
+import createGroup from "./createGroup";
+import endDate from "./endDate";
+import giftRecipientList from "./giftRecipientList";
+import removeUserFromGroup from "./removeUserFromGroup";
+import setEndDate from "./setEndDate";
+import userList from "./userList";
 
-export { telegram };
+export {
+  addUserToGroup,
+  assignGiftRecipient,
+  createGroup,
+  endDate,
+  giftRecipientList,
+  removeUserFromGroup,
+  setEndDate,
+  userList,
+};
 
 export default {
-  telegram,
+  addUserToGroup,
+  assignGiftRecipient,
+  createGroup,
+  endDate,
+  giftRecipientList,
+  removeUserFromGroup,
+  setEndDate,
+  userList,
 };
