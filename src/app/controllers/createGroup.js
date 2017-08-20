998bcfff3a097e18958bd6f624bddf1d41f20dd6
Liau Jian Jie <hello@jianjie.co>
2017-08-21 05:06:31 +0800

    Implement _createGroup controller HOC

--- src/app/controllers/createGroup.js
+++ src/app/controllers/createGroup.js
@@ -1,3 +1,18 @@
-export default function createGroup() {
-  console.log("create group");
-}
+import Group from "app/models/Group";
+// import User from "app/models/User";
+import ERROR from "lib/errors";
+
+const _createGroup = platform => async identifier => {
+  try {
+    const existingGroup = await Group.findOne({ platform, identifier });
+    if (existingGroup) {
+      throw ERROR.GROUP_ALREADY_EXISTS;
+    }
+    const newGroup = new Group({ platform, identifier });
+    await newGroup.save();
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default _createGroup;
--- src/app/interfaces/telegram.js
+++ src/app/interfaces/telegram.js
@@ -2,12 +2,31 @@ import TeleBot from "telebot";
 import { TELEBOT_CONFIG } from "config";
 import { commandNotFound } from "lib/utils/responses";
 import { telegram as telegramRoutes } from "app/routes";
+import controller from "app/controllers";
 import { logger } from "lib/utils";
+import { ERROR } from "lib";
 
 const telegramBot = new TeleBot(process.env.TELEGRAM_TOKEN);
 
 telegramBot.on("start", () => {
   logger.info("Bot now polling for updates...");
+});
+
+telegramBot.on("/start", async message => {
+  try {
+    await controller.createGroup("telegram")(message.chat.id);
+    message.reply.text(
+      "Ho ho ho! A secret santa game has been started! Type /join to participate.",
+    );
+  } catch (error) {
+    switch (error) {
+      case ERROR.GROUP_ALREADY_EXISTS:
+        message.reply.text(
+          "The game has already been started! Type /join to participate.",
+        );
+        break;
+    }
+  }
 });
 
 // Sample logging. Add what you think should get logged.
@@ -30,7 +49,7 @@ message-received: ${message.text}
     const response = commandNotFound();
     return message.reply.text(response);
   }
-  message.reply.text(message.text);
+  // message.reply.text(message.text);
 });
 
 export default telegramBot;
