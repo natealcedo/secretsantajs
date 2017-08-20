eba0103a9bf206f63111972b0b93d560abf23c0f
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2017-08-20 22:59:24 +0800

    Add logic to message received event

--- src/app/interfaces/telegram.js
+++ src/app/interfaces/telegram.js
@@ -1,5 +1,7 @@
 import TeleBot from "telebot";
 import { TELEBOT_CONFIG } from "config";
+import { commandNotFound } from "lib/utils/responses";
+import { telegram as telegramRoutes } from "app/routes";
 import { logger } from "lib/utils";
 
 const telegramBot = new TeleBot(process.env.TELEGRAM_TOKEN);
@@ -16,6 +18,18 @@ date-time: ${message.date}
 chat-type: ${message.chat.type}
 message-received: ${message.text}
   `);
+  /*
+   * TODO: Implement a parser to parse input.
+   * Eg: If the input command is wrong, show an error.
+   * Or if command is correct but second command is wrong still need
+   * to respond in a more friendly manner.
+   *
+   */
+  const userInput = message.text.toLowerCase();
+  if (!telegramRoutes.includes(userInput)) {
+    const response = commandNotFound();
+    return message.reply.text(response);
+  }
   message.reply.text(message.text);
 });
 
