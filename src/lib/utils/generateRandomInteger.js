a190d6b7e6c055a8459f665faa60bb8eb87bb222
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2017-08-20 22:10:07 +0800

    Rename function from generateRandomFunction to generateRandomInteger

--- src/lib/utils/generateRandomInteger.js
+++ src/lib/utils/generateRandomInteger.js
@@ -3,14 +3,14 @@
  */
 
 /**
- * generateRandomFunction
+ * generateRandomInteger
  *
  * @private
  * @param {Number} lowerBound Lower bound of random number generator.
  * @param {Number} upperBound Upper bound of number generator.
  * @returns {Number} Random integer number inclusive between lowerBound and upperBound.
  */
-export default function generateRandomFunction(lowerBound, upperBound) {
+export default function generateRandomInteger(lowerBound, upperBound) {
   const min = Math.ceil(lowerBound);
   const max = Math.floor(upperBound);
   return Math.floor(Math.random() * (max - min + 1)) + min;
