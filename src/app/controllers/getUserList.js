c33ee38f787668bf1d038c008edef4892ee8f866
Liau Jian Jie <hello@jianjie.co>
2017-08-23 01:18:29 +0800

    Implement getUserList controller method

--- src/app/controllers/getUserList.js
+++ src/app/controllers/getUserList.js
@@ -1,3 +1,12 @@
-export default function giftRecipientList() {
-  console.log("Assign gift recipient list");
-}
+import { Group } from "app/models";
+import { errors } from "lib";
+
+const _getUserList = platform => async identifier => {
+  const group = await Group.findOne({ identifier });
+  if (!group) {
+    throw errors.GROUP_DOES_NOT_EXIST;
+  }
+  return group.users;
+};
+
+export default _getUserList;
--- src/app/interfaces/telegram.js
+++ src/app/interfaces/telegram.js
@@ -2,19 +2,27 @@ import TeleBot from "telebot";
 import { TELEBOT_CONFIG } from "config";
 import { commandNotFound } from "lib/utils/responses";
 import { telegram as telegramRoutes } from "app/routes";
-import controller from "app/controllers";
+import _controller from "app/controllers";
 import { logger } from "lib/utils";
 import { errors } from "lib";
 
-const telegramBot = new TeleBot(process.env.TELEGRAM_TOKEN);
+// Expose platform to controller
+const INTERFACE = "telegram";
+const controller = Object.keys(_controller).reduce((acc, key) => {
+  acc[key] = _controller[key](INTERFACE);
+  return acc;
+}, {});
 
+// Instantiate bot instance
+const telegramBot = new TeleBot(process.env.TELEGRAM_TOKEN);
+// Log once when bot starts
 telegramBot.on("start", () => {
   logger.info("Bot now polling for updates...");
 });
 
 telegramBot.on("/start", async message => {
   try {
-    await controller.createGroup("telegram")(message.chat.id);
+    await controller.createGroup(message.chat.id);
     message.reply.text(CREATE_GROUP_SUCCESS);
   } catch (error) {
     handleError(error, message);
@@ -23,12 +31,27 @@ telegramBot.on("/start", async message => {
 
 telegramBot.on("/join", async message => {
   try {
-    await controller.addUserToGroup("telegram")(
-      message.chat.id,
-      message.chat.id,
-    );
+    await controller.addUserToGroup(message.from.id, message.chat.id);
     const fullName = nameFromObject(message.from);
     message.reply.text(JOIN_GROUP_SUCCESS.replace("$0", fullName));
+  } catch (error) {
+    handleError(error, message);
+  }
+});
+
+telegramBot.on("/list", async message => {
+  try {
+    const userIdList = await controller.getUserList(message.chat.id);
+    const userPromises = await userIdList.map(async userId =>
+      telegramBot.getChatMember(message.chat.id, userId),
+    );
+    const rawUsers = await Promise.all(userPromises);
+    const users = rawUsers.map(value => value.result.user);
+    const fullNames = users.map(user => nameFromObject(user));
+    const namesWithIndex = fullNames.map(
+      (name, index) => `\n${index + 1}. ${name}`,
+    );
+    message.reply.text(`Secret santa participants: ${namesWithIndex}`);
   } catch (error) {
     handleError(error, message);
   }
@@ -42,19 +65,6 @@ date-time: ${message.date}
 chat-type: ${message.chat.type}
 message-received: ${message.text}
   `);
-  /*
-   * TODO: Implement a parser to parse input.
-   * Eg: If the input command is wrong, show an error.
-   * Or if command is correct but second command is wrong still need
-   * to respond in a more friendly manner.
-   *
-   */
-  const userInput = message.text.toLowerCase();
-  if (!telegramRoutes.includes(userInput)) {
-    const response = commandNotFound();
-    return message.reply.text(response);
-  }
-  // message.reply.text(message.text);
 });
 
 const handleError = (error, message) => {
