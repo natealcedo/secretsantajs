6f57432b7b4fe86f52343588f4869acd002f18d7
Liau Jian Jie <hello@jianjie.co>
2017-09-02 02:49:36 +0800

    Add more response constants

--- src/lib/responses/index.js
+++ src/lib/responses/index.js
@@ -2,15 +2,24 @@ const TALK_TO_BOT =
   "Hey $0, please send me a private message at @HohohohoBot so that I can reveal your gift recipient to you later!";
 const RECEIPIENT_REVEAL = "Your secret santa gift receipient from $0 is $1.";
 const NO_PARTICIPANTS =
-  "No one has joined the secret santa yet! Type /join to participate.";
-const LIST_PARTICIPANTS = "Secret santa participants: $0";
+  "No one has joined the secret santa game yet! Type /join to participate.";
+const LIST_PARTICIPANTS = "Secret santa game participants: $0";
 const CREATE_GROUP_SUCCESS =
-  "Ho ho ho! A secret santa has been started! Type /join to participate.";
-const JOIN_GROUP_SUCCESS = "$0 has joined the secret santa.";
-const LEAVE_GROUP_SUCCESS = "$0 has left the secret santa.";
+  "Ho ho ho! A secret santa game has been started! Type /join to participate.";
+const JOIN_GROUP_SUCCESS = "$0 has joined the secret santa game.";
+const LEAVE_GROUP_SUCCESS = "$0 has left the secret santa game.";
 const LIST_PARTICIPANTS_AND_SANTAS = "Ho ho ho! Here are your pairings:\n$0";
 const END_GROUP_SUCCESS =
-  "$0 has ended the secret santa. Type /start to start another secret santa!";
+  "$0 has ended the secret santa game. Type /start to start another secret santa game!";
+const HELP_INFO_GROUP = `<b>Here are the available commands:</b>
+ /start - starts a secret santa game
+ /join - adds you to the secret santa game
+ /leave - remove you from the secret santa game
+ /list - list all the participants of the secret santa game
+ /assign - assign a random gift recipient to all participants
+ /reveal - reveals all the secret santas and their gift recipients
+ /end - ends the secret santa game
+ /info - gives information about the bot`;
 
 export default {
   TALK_TO_BOT,
@@ -22,4 +31,5 @@ export default {
   LEAVE_GROUP_SUCCESS,
   LIST_PARTICIPANTS_AND_SANTAS,
   END_GROUP_SUCCESS,
+  HELP_INFO_GROUP,
 };
